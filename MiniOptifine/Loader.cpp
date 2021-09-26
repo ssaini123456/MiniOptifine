7ef2a1ba81c56848c09aed1f1eaa5c207617f458
Sutinder S. Saini <sutinder456@gmail.com>
2021-09-26 16:23:38 -0700

    fix + update offset chain
    * Might as well update this while i'm here

--- MiniOptifine/Loader.cpp
+++ MiniOptifine/Loader.cpp
@@ -1,51 +1,58 @@
 #include "Loader.h"
 
 bool g_isToggled = false;
+std::string bootLogo = R"(
+           __ __ __      __ __ __ 
+          /_ /_ /_ |    /_ /_ /_ |
+  ___  __ _| || || |_ __ | || || |
+ / __|/ _` | || || | '_ \| || || |
+ \__ \ (_| | || || | | | | || || |
+ |___/\__,_|_||_||_|_| |_|_||_||_|
+                                  
+@github: https://github.com/sa111n111 
+@twitter: https://twitter.com/sa111n111            
+)";
 
 void init() {
     GameUtil* l;
+    float zoomVal = 0;
+    float defaultFov = 0;
     uint32_t process_id = l->getGameProcessId();
     uintptr_t gameId = l->getGameModule(process_id);
     HANDLE process_handle = OpenProcess(PROCESS_ALL_ACCESS, NULL, process_id);
-    std::cout << "Checking if game window is present..." << std::endl;
+    uintptr_t optifineZoombaseAddress = gameId + 0x040858C8;
+    uintptr_t __optifineZoomPtr = l->findAddressFromPointer(process_handle, optifineZoombaseAddress, { 0x28, 0xCC0, 0x08, 0x50, 0x50, 0x128, 0x18 });
 
+
+    std::cout << bootLogo << "\n";
+    std::cout << "Checking if game window is present...\n\n";
     // check if the minecraft window is even open
     HWND gameWindow = FindWindow(NULL, L"Minecraft");
     if (!gameWindow) {
         system("color 4");
-        std::cout << "!!!! Game window not found. Please open minceraft and try again.";
+        std::cout << "!!!! Game window not found. Please open minceraft and try again.\n\n";
         return;
     }
     else {
         system("color b");
-        std::cout << "[~] Game window found! To toggle optifine zoom press C." << std::endl;
+        std::cout << "[~] Game window found! To toggle optifine zoom press C.\n";
     }
-
-    // k, begin writing to the ptr
-    uintptr_t optifineZoombaseAddress = gameId + 0x03F58580;
-    uintptr_t __optifineZoomPtr = l->findAddressFromPointer(process_handle, optifineZoombaseAddress, { 0x28, 0xFC8, 0x8, 0xC8, 0x170, 0x128, 0x18 });
-    float zoomVal;
+    ReadProcessMemory(process_handle, (unsigned int*)__optifineZoomPtr, &defaultFov, sizeof(defaultFov), nullptr); /* copy what we read from the process to the defaultFov buffer */
 
     while (true) {
+        Sleep(100);
         if (GetAsyncKeyState('C') & 0x8000) {
-            if (g_isToggled == false) {
-                zoomVal = 10; // max zoom
+            g_isToggled = !g_isToggled;
+            
+            if (g_isToggled == true) {
+                zoomVal = 11;
                 WriteProcessMemory(process_handle, (unsigned int*)__optifineZoomPtr, &zoomVal, sizeof(zoomVal), nullptr);
-                g_isToggled = true;
-                Sleep(100);
+                
+            } else {
+                //std::cout << std::dec << defaultFov << "\n";
+                WriteProcessMemory(process_handle, (unsigned int*)__optifineZoomPtr, &defaultFov, sizeof(defaultFov), nullptr);
             }
         }
-
-        if(GetAsyncKeyState('C') & 0x8000) {
-            // write defaults
-            if (g_isToggled == true) {
-                zoomVal = 60;
-                WriteProcessMemory(process_handle, (unsigned int*)__optifineZoomPtr, &zoomVal, sizeof(zoomVal), nullptr);
-                g_isToggled = false;
-                Sleep(100);
-            }
-            
-        }    
     }
 
 }
--- MiniOptifine/Util/GameUtil.h
+++ MiniOptifine/Util/GameUtil.h
@@ -11,6 +11,4 @@ public:
 	static uintptr_t findAddressFromPointer(HANDLE proc, uintptr_t provided_ptr, std::vector<uint32_t> provided_offsets);
 	static uint32_t	 getGameProcessId();
 	static uintptr_t getGameModule(uint32_t processId);
-
 };
-
